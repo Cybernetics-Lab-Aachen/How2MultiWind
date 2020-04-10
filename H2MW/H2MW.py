ff6e5ca8b7b6c9bd5cb8c8b64c62e919e7fefa66
arlinddamjani <arlind.damjani@rwth-aachen.de>
2020-04-10 21:07:46 +0200

    Erste Kommentare zur Organisierung

--- H2MW/H2MW.py
+++ H2MW/H2MW.py
@@ -1,1 +1,84 @@
-# Test 2 von 3
+"""
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                           How 2 MultiWind - Wickelmaschinendemonstrator Anwendung
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ Index:
+ Maschine 1 - Flechtmaschine
+ Maschine 2 - Multifilamentwickelmaschine 90
+ Maschine 3 - Mulftifilamentwickelmaschine 48
+ Maschine 4 - Nasswickelmaschine
+
+"""
+
+" TO DO: Deklaration der Parameter "
+
+# Maschine 1
+
+
+
+# Maschine 2
+
+
+
+# Maschine 3
+
+
+
+# Maschine 4
+
+
+
+" TO DO: Checks für Gültigkeit der Werte im Verhältnis zu den erlaubten Grenzen "
+
+# Maschine 1
+
+
+
+# Maschine 2
+
+
+
+# Maschine 3
+
+
+
+# Maschine 4
+
+
+
+ " TO DO: Ausrechnung der Nachhaltigkeitswerte "
+
+# Maschine 1
+
+
+
+# Maschine 2
+
+
+
+# Maschine 3
+
+
+
+# Maschine 4
+
+
+
+" Ausgabe der Nachhaltigkeitswerte"
+
+# Maschine 1
+
+
+
+# Maschine 2
+
+
+
+# Maschine 3
+
+
+
+# Maschine 4
+
+
